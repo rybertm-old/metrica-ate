84122e78cd8ff3c399301af22eb46fd59301be91
Guilherme Senna Cruz <53920452+GuilhermeSenna@users.noreply.github.com>
2021-07-07 22:06:28 -0300

    Correção na declaração das variáveis

--- metricas/p_uso.js
+++ metricas/p_uso.js
@@ -3,16 +3,19 @@
 
 // S, T, V, O, a, b, q, r, z
 function p_uso(S, T, V, operador, operando_a, operando_b, operando_rel, res_sim, res_com) {
-    let SOMc_uso = operadores = proposicoes = 0;
+    let SOMc_uso = 0;
 
     // Σ; n = 1, n <= S...
     for (let n = 1; n <= S; n++) { // Quantidade de predicados
 
+        let operadores = 0;
         // Σ; m = 1, m <= T...
         for (let m = 1; m <= T; m++) { // Quantidade de operadores
             operadores += operador[m];
         }
 
+        let proposicoes = 0;
+        
         // Σ; f = 1, f <= V...
         for (let f = 1; f <= V; f++) { // Quantidade de proposições
             proposicoes += operando_a + operando_b + operando_rel + res_sim;
