836d93e4f53d34bc5ad3953abf69c32b18846fc5
zBiggui <bigguisenna@gmail.com>
2021-07-08 13:28:17 -0300

    Alterando nome da variável

--- metricas/p_uso.js
+++ metricas/p_uso.js
@@ -3,7 +3,7 @@
 
 // S, T, V, O, a, b, q, r, z
 function p_uso(S, T, V, operador, operando_a, operando_b, operando_rel, res_sim, res_com) {
-    let SOMc_uso = 0;
+    let SOMp_uso = 0;
 
     // Σ; n = 1, n <= S...
     for (let n = 1; n <= S; n++) { // Quantidade de predicados
@@ -15,15 +15,15 @@ function p_uso(S, T, V, operador, operando_a, operando_b, operando_rel, res_sim,
         }
 
         let proposicoes = 0;
-        
+
         // Σ; f = 1, f <= V...
         for (let f = 1; f <= V; f++) { // Quantidade de proposições
             proposicoes += operando_a + operando_b + operando_rel + res_sim;
         }
 
-        SOMc_uso += operadores + proposicoes + res_com;
+        SOMp_uso += operadores + proposicoes + res_com;
     }
 
     // Retorna como string formatado em Hexadecimal
-    return SOMc_uso.toString(16);
+    return SOMp_uso.toString(16);
 }
