f5f6b0aa48ef01445fc019c7b45a3ed12959ccc4
Victor José Gonçalves Martins <42471720+Vict-Martins@users.noreply.github.com>
2021-07-07 22:57:37 -0300

    Caminho.js && caminho.ejs
    configurando 'caminho.js' e 'caminho.ejs'

---
+++ routes/caminho.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var dbConn = require('../db/db.js');
+
+
+// ROTA PARA mostrar a pagina index.ejs, no evento da pasta raiz '/'
+router.get('/', function (req, res, next) {
+    dbConn.query('SELECT * from caminho', function (err, queryTeste) {
+        if (err) {
+            req.flash('error', err.message);
+            // preparar dados para página em views/metrica/index.ejs. 'metrica' é a pasta em view
+            res.render('metrica/casoteste.ejs', { dataProduto: '' });
+        } else {
+            // preparar dados para página em views/editora/index.ejs
+            res.render('metrica/casoteste.ejs', { dataProduto: queryTeste });
+        }
+    });
+});
+
+module.exports = router;
